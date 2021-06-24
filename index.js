067eedf7de6ce15b1ca8da36326fbec8ac860a76
Van Vo <54080467+vandevo@users.noreply.github.com>
2021-06-24 15:22:25 +0100

    master

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 let messageEl = document.getElementById('input-btn')
+let theBox = document.getElementById('openBox')
 
 function clickButton() {
     messageEl.textContent = "Button Clicked"
+}
+
+theBox.addEventListener("click", function(){
+    console.log("Quero abrir esta caixa")
 }

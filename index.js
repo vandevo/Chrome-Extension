334e1f87e52c209afdefb68199f981b77078f0e6
Van Vo <54080467+vandevo@users.noreply.github.com>
2021-06-27 18:54:01 +0100

    master

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputBtn = document.getElementById('input-btn')
 const inputEl = document.getElementById('input-el')
 const ulEl = document.getElementById('ul-el')
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
-    inputBtn.textContent = myLeads
+    // inputBtn.textContent = myLeads
+    renderLeads()
 })
 
-let listItems = ""
-for (let i = 0 ; i < myLeads.length ; i++){
-    listItems += "<li>" + (myLeads[i]) + "</li>"
+function renderLeads(){
+    let listItems = ""
+    for (let i = 0 ; i < myLeads.length ; i++){
+        listItems += "<li>" + (myLeads[i]) + "</li>"
+        // const li = document.createElement("li")
+        // li.textContent = myLeads[i]
+        // ulEl.append(li)
+    }
 
-    // const li = document.createElement("li")
-    // li.textContent = myLeads[i]
-    // ulEl.append(li)
-
+    ulEl.innerHTML = listItems
 }
-
-ulEl.innerHTML = listItems
-
 
 
 let theBox = document.getElementById('openBox')

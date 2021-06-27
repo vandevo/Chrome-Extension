3293cf400339f41b605d554d07ae824539f89348
Van Vo <54080467+vandevo@users.noreply.github.com>
2021-06-27 18:58:37 +0100

    master

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const ulEl = document.getElementById('ul-el')
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
+    inputEl.value = ""
     // inputBtn.textContent = myLeads
     renderLeads()
 })

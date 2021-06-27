0e70ef202054ce38a965e9f931411d78d5318490
Van Vo <54080467+vandevo@users.noreply.github.com>
2021-06-27 19:18:54 +0100

    master

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ inputBtn.addEventListener("click", function(){
 function renderLeads(){
     let listItems = ""
     for (let i = 0 ; i < myLeads.length ; i++){
-        listItems += "<li>" + (myLeads[i]) + "</li>"
+        listItems += "<li><a target='_blank' href=' " + (myLeads[i])+ " '>" + (myLeads[i]) + "</a></li>"
         // const li = document.createElement("li")
         // li.textContent = myLeads[i]
         // ulEl.append(li)

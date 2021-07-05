e858485617159d92707bf28170f6e7712c8b9953
Van Vo <54080467+vandevo@users.noreply.github.com>
2021-07-05 16:56:29 +0100

    master

--- index.js
+++ index.js
@@ -19,15 +19,16 @@ if (leadsFromLocalStorage) {
 }
 
 tabBtn.addEventListener("click", function(){
-    // chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-    // let url = tabs[0].url})
-
-    chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+    // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    // })
+    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
         myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
         render(myLeads)
-    }})
+    })
 
+    
 })
 
 function render(leads){
